7c61d122e99fd6974bc17b317bb4a45ced46d4ca
Richard S Hill <hello@richardshill.co.uk>
2016-02-20 13:01:07 +0000

    First data commit - new xCode set up files for dev. 01.00am 20.02.16

---
+++ railawayIOS/railawayIOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  railawayIOS
+//
+//  Created by richardmacbookpro on 20/02/2016.
+//  Copyright (c) 2016 richardshill. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
